41214827647363874bbc3d5087f2eb9d4b70b14d
kmd178 <kmd178@gmail.com>
2017-02-26 17:41:30 +0200

    Step1 Complete (4bit Gray Counter)

--- 1.4-bit Gray/Gray_4bits_RTL.v
+++ 1.4-bit Gray/Gray_4bits_RTL.v
@@ -1,40 +1,23 @@
-                                              -
- /*
- -----------------------------------------------------------------------------
- -- File           : Gray_4bits_RTL.v
- -----------------------------------------------------------------------------
- -- Description    : Verilog code for the 4-bit Gray 
- --  with asynchronous reset
- -- 4 bit gray code :
-  --  bin     hex
-  --  0000    0
-  --  0001    1
-  --  0011    3
-  --  0010    2
-  --  0110    6
-  --  0111    7
-  --  0101    5
-  --  0100    4
-  --  1100    C
-  --  1101    D
-  --  1111    F
-  --  1110    E
-  --  1010    A
-  --  1011    B
-  --  1001    9
-  --  1000    8
- -- --------------------------------------------------------------------------
- */ 
-
-module gray_4bits (clk, clk_en, rst, gray_out)
+//-- Description    : Verilog code for the 4-bit Gray --  with asynchronous reset
+module gray_4bits (clk, clk_en, rst, state);
   input clk, clk_en, rst;
-  reg [3:0] state;
-  wire gray_out;
+  output reg [3:0] state; //Gray counter register.
+  reg auxbit; //Auxiliary bit necessary for the implementation of the 4bit-Gray counter.
 
   always @(posedge clk or rst) 
    begin
-     // Code to implement a 4-bit Gray code
-     
-    assign gray_out = state;
+     if(rst) begin
+     	state[3:0] <=4'b0000;
+     	auxbit<=1;
+     end 
+     else begin
+     //4bit-Gray counter logic.
+     	auxbit<= ~auxbit;
+     	state[0]<= auxbit^state[0];
+     	state[1]<= (state[0]&&(~auxbit))^state[1];
+     	state[2]<=(state[1]&&(~(state[0]||auxbit)))^state[2];
+     	state[3]<=(~(state[0]||auxbit||state[1]))^state[3];
+     end
+  end
   
-end module
+endmodule
--- 1.4-bit Gray/TB1_Gray.v
+++ 1.4-bit Gray/TB1_Gray.v
@@ -1,39 +1,53 @@
- /*                                -
- -----------------------------------------------------------------------------
- -----------------------------------------------------------------------------
- -- File           : TB1_Gray.v
- -----------------------------------------------------------------------------
- -- Description    : Verilog testbench for Gray_4bits
- -- --------------------------------------------------------------------------
- -- --------------------------------------------------------------------------
- */
- 
+//-- Description    : Verilog code for the 4-bit Gray --  with asynchronous reset
 `timescale 1ns/1ps
-`define cycle 10
+ `define cycle 10
+ module TB1;
+   
+   reg clk, rst, clk_en;
+   wire [3:0] leds;
+   //integer EndOfSimulation;  
+   integer i;
+ 
+   parameter ONE = 1'b1; 
+                              
+   // Initial statement for signal initialization (reset, clk)
+    initial 
+      begin
+           clk=1'b0;
+           rst=1'b1;
+           #(4.25*`cycle) rst = 1'b0; //Instanciating asynchronous reset signal
+           for (i = 0; i < 15; i=i +1) 
+                begin
+                    $display("na to %b",leds);
+                    #(`cycle);
+                end
+           $stop;
+      end
+      
+   // Always statement to drive the clock goes here
+   always 
+        #(`cycle / 2) clk=~clk;
+    
+   gray_4bits DUT(.clk(clk), .clk_en(ONE), .rst(rst), .state(leds));   
+   
+ endmodule
 
-module TB1;
-  
-  reg clk, rst, clk_en;
-  reg [3:0] leds;
-  integer EndOfSimulation;  
-  integer i;
-
-  parameter ONE = 1'b1; 
-							 
-  // Initial statement for signal initialization (reset, clk, EndOfSimulation)
-   initial 
-     begin
-      // ....
-	  $finish
-     end;
-	 
-  // Always statement to drive the clock goes here
-  always 
-    begin
-	// ...
-    end
-	
-   // Instantiation of the gray_4bits 
-  gray_4bits DUT(.clk(clk), .clk_en(ONE), .rst(rst), .gray_out(leds));   
-  
-end module;
+ /* Expacted Output:
+  --  bin     hex
+  --  0000    0
+  --  0001    1
+  --  0011    3
+  --  0010    2
+  --  0110    6
+  --  0111    7
+  --  0101    5
+  --  0100    4
+  --  1100    C
+  --  1101    D
+  --  1111    F
+  --  1110    E
+  --  1010    A
+  --  1011    B
+  --  1001    9
+  --  1000    8
+ */ 
