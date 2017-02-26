4d82a61141ffb220d34ef03a53cd44327152c739
kmd178 <kmd178@gmail.com>
2017-02-26 23:06:58 +0200

    Step2 Complete. (N-bit Gray)

--- 2.N-bit Gray/TB2_Gray.v
+++ 2.N-bit Gray/TB2_Gray.v
@@ -1,21 +1,10 @@
- /*                                -
- -----------------------------------------------------------------------------
- -----------------------------------------------------------------------------
- -- File           : TB1_Gray.v
- -----------------------------------------------------------------------------
- -- Description    : Verilog testbench for Gray_4bits
- -- --------------------------------------------------------------------------
- -- --------------------------------------------------------------------------
- */
- 
 `timescale 1ns/1ps
 `define cycle 10
 
 module TB2;
   parameter N = 4;
   reg clk, rst, clk_en;
-  reg [3:0] leds;
-  integer EndOfSimulation;  
+  wire [N-1:0] leds;
   integer i;
 
   parameter ONE = 1'b1; 
@@ -23,17 +12,22 @@ module TB2;
   // Initial statement for signal initialization (reset, clk, EndOfSimulation)
    initial 
      begin
-      // ....
-	  $finish
-     end;
+      clk=1'b0;
+      rst=1'b0;
+      #(4.25*`cycle) rst = 1'b1;
+      for (i = 0; i < 15; i=i +1) 
+          begin
+            $display("na to %b",leds);
+            #(`cycle);
+          end
+     end
 	 
-  // Always statement to drive the clock goes here
+  // Always statement to drive the clock
   always 
-    begin
-	// ...
-    end
-	
-   // Instantiation of the gray_Nbits goes here
+	   #(`cycle / 2) clk=~clk;
+   
+
+   // Instantiation of the gray_Nbits 
+  gray_Nbits DUT(.clk(clk), .clk_en(ONE), .rst(rst), .gray_out(leds));   
   
-  
-end module;
+endmodule
