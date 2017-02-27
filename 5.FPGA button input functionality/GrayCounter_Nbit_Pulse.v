0f6074e7df61e92e441b1ecc38313e8ec4bc98e7
kmd178 <kmd178@gmail.com>
2017-02-27 22:33:57 +0200

    Step5 Initialization

--- 5.FPGA button input functionality/GrayCounter_Nbit_Pulse.v
+++ 5.FPGA button input functionality/GrayCounter_Nbit_Pulse.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:17:55 02/04/2010 
-// Design Name: 
-// Module Name:    GrayCounter_Nbit_Pulse 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
 // Description: This FSM converts an input level signal to a single-cycle pulse. 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module GrayCounter_Pulse(input clk, input rst, input level, output reg pulse);
 
    reg [1:0] state;
--- 5.FPGA button input functionality/GrayCounter_System.v
+++ 5.FPGA button input functionality/GrayCounter_System.v
@@ -1,35 +1,15 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:50:26 02/04/2010 
-// Design Name: 
-// Module Name:    GrayCounter_System 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
+`timescale 1ns / 100ps
 // Additional Comments: Instantiation of the GrayCounter_Nbits and GrayCounter_Pulse models
-//
-//////////////////////////////////////////////////////////////////////////////////
 module GrayCounter_System(clk, rst, leds);
   parameter N = 4;
-  parameter distance = // How much for 1 Hz when clk period is 10 ns?
+  //parameter distance = // How much for 1 Hz when clk period is 10 ns?
   input clk, rst;
   output [N-1:0] leds;
   wire pulse;
     
-  // Instantiation of the GrayCounter_Pulse 
-  // Here
-	 
-  // Instantiation of the gray_Nbits 
-  // Here
+  GrayCounter_Pulse SecondPulse (clk, rst, pulse);// Instantiation of the GrayCounter_Pulse 
+ 
+  gray_Nbits Counter(pulse, 1'b0 , rst, leds);// Instantiation of the gray_Nbits 
 
 
 endmodule
